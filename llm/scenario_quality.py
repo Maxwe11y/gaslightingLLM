3bcd53c98becd770a264b8f0371e4fb81bfa95fd
Maxwe11y <wei008@e.ntu.edu.sg>
2024-01-23 19:26:19 +0800

    second commit

--- llm/scenario_quality.py
+++ llm/scenario_quality.py
@@ -1,22 +1,14 @@
 # Author: Wei Li
 # Email: wei.li@nus.edu.sg
-import random
 
-# import torch
-# from torch.utils.data import Dataset
-# from torch.nn.utils.rnn import pad_sequence
-# import pickle
-# import pandas as pd
-# import numpy as np
-# from torch.utils.data.sampler import SubsetRandomSampler
-# from torch.utils.data import DataLoader
-
-import openai
 from openai import OpenAI
 import tiktoken
 from utils import TokenPricer
 import os
-
+import inflect
+import random
+import string
+import re
 def num_tokens_from_string(string, encoding_name):
     encoding = tiktoken.get_encoding(encoding_name)
     num_tokens = len(encoding.encode(string))
@@ -29,10 +21,6 @@ client = OpenAI(
     # This is the default and can be omitted
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
-max_tok = 1700
-
-
-import inflect
 
 def number_to_ordinal_words(number):
     if number not in ['N', 0]:
@@ -54,34 +42,101 @@ def load_data(filename, path="./data/"):
 
 if __name__ == '__main__':
 
-    scenarios = load_data('sce_topic_final.txt', path='./data')
-    # get response from ChatGPT
-    for scenario in scenarios:
-        # chat = [{"role": 'system', "content": scenario}]
+    max_tok = 2000
 
-        Profile = "You are expert Alex. You are supposed to examine or evaluate several script scenes that happened in one person or between two persons." \
-                  "Here are some examples of good script scenes: 'Max is goign to perform at a concert.', 'John's design is rejected by the magzaine.', 'Oliver is admitted to his dream university.'," \
-                  "'Tom got promoted at the company.', 'Cynthia did not pass the math exam at the end of last term.' " \
-                  "You have to carefully determine whether a script scene is good or bad. If good,  If not good, please modify the scene according to the above examples. The output should be in the following format:" \
-                  + "[The script scene] {}".format(scenario) \
-                  + "[Judgment]" \
-                  + "[Modification]"
+    # scenarios = load_data('sce_topic_final.txt', path='./data')
+    # # get response from ChatGPT
+    # for scenario in scenarios:
+    #     # chat = [{"role": 'system', "content": scenario}]
+    #
+    #     Profile = "You are expert Alex. You are supposed to examine or evaluate several script scenes that happened in one person or between two persons." \
+    #               "Here are some examples of good script scenes for your reference: 'Max is going to perform at a concert.', 'John's design is rejected by the magzaine.', 'Oliver is admitted to his dream university.'," \
+    #               "'Tom got promoted at the company.', 'Cynthia did not pass the math exam at the end of last term.' " \
+    #               "You have to carefully determine whether a script scene is good or bad based on the good script scenes above. If good, please fill [Modification] with NA. If bad, please modify the scene using the aforementioned good script scenes as references. " \
+    #                "The [Modification] should be precise and short. Given a script scenario below, please fill the Judgment. Please do not fill the modification unless the Judgment is bad. \n" \
+    #               + "[The script scene] {} \n".format(scenario) \
+    #               + "[Judgment] \n" \
+    #               + "[Modification]"
+    #
+    #
+    #     query = "Please fill the Judgement. Please do not fill the modification unless the Judgment is bad."
+    #
+    #     chat_completion = client.chat.completions.create(
+    #         messages=[
+    #             {
+    #                 "role": "system",
+    #                 "content": Profile,
+    #             }
+    #         ],
+    #         model="gpt-3.5-turbo",
+    #     )
+    #     res = chat_completion.choices[0].message.content
+    #
+    #     tokens = TokenPricer()
+    #     print(tokens.gpt_get_estimated_cost(res, max_tokens=0))
+    #     print(res)
 
-        query = "Please fill the Judgement. Please do not fill the modification unless the judgment is bad."
+
+    generated_scenarios = []
+
+    example_hints = ['Max is going to perform at a concert.', "John's design is rejected by the magzaine.",
+                     'Oliver is admitted to his dream university', 'Tom got promoted at the company.', 'Cynthia did not pass the math exam at the end of last term.']
+
+    num_gen = 20
+
+    target = 100
+
+    num_hints = 5
+
+    while len(generated_scenarios) <= target:
+
+        Profile_new = "You are expert Alex. You are supposed to provide several script scenes." \
+                      "Here are some examples of good script scenes for your reference: {} ".format(example_hints) \
+                      + (" Please generate {} good script scenes using the aforementioned good script scenes as references."
+                         "The outputs should be separated by newline character '\\n'. Please do not include any number in front of the generated script scene. \n").format(
+            num_gen)
 
         chat_completion = client.chat.completions.create(
             messages=[
                 {
                     "role": "system",
-                    "content": Profile + scenario,
+                    "content": Profile_new,
                 }
             ],
             model="gpt-3.5-turbo",
         )
-        res = chat_completion.choices[0].message.content
 
-        tokens = TokenPricer()
-        print(tokens.gpt_get_estimated_cost(res, max_tokens=0))
-        print(res)
+        results = chat_completion.choices[0].message.content
+
+        scenarios = results.split('\n')
+        scenarios = [sce for sce in scenarios if sce != ""]
+
+        if len(scenarios) == num_gen and isinstance(scenarios, list):
+            hints = random.sample(scenarios, num_hints)
+        else:
+            print('generation exception')
+            print('scenarios', scenarios)
+            print('results', results)
+            continue
+
+        example_hints = ' '.join(hints)
+
+        generated_scenarios.extend(scenarios)
+
+    with open('./data/gpt_generated_scenarios.txt', 'w') as f:
+        f.writelines('\n'.join(generated_scenarios))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     print("done!")
