e3850fb9b0f1260bedf6a48aa11fbcffda17d449
Maxwe11y <wei008@e.ntu.edu.sg>
2024-01-24 20:45:40 +0800

    second commit

--- llm/scenario_quality.py
+++ llm/scenario_quality.py
@@ -1,7 +1,7 @@
 # Author: Wei Li
 # Email: wei.li@nus.edu.sg
 
-from openai import OpenAI
+from openai import OpenAI, AsyncOpenAI
 import tiktoken
 from utils import TokenPricer
 import os
@@ -9,6 +9,8 @@ import inflect
 import random
 import string
 import re
+import asyncio
+import numpy as np
 def num_tokens_from_string(string, encoding_name):
     encoding = tiktoken.get_encoding(encoding_name)
     num_tokens = len(encoding.encode(string))
@@ -17,7 +19,12 @@ def num_tokens_from_string(string, encoding_name):
 # OPENAI_API_KEY = "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" # luyao
 # openai.api_key = "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" # wei
 
-client = OpenAI(
+# client = OpenAI(
+#     # This is the default and can be omitted
+#     api_key=os.environ.get("OPENAI_API_KEY"),
+# )
+
+client = AsyncOpenAI(
     # This is the default and can be omitted
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
@@ -38,6 +45,42 @@ def load_data(filename, path="./data/"):
             data.append(line.strip())
 
     return data
+
+
+def check_scenario(scenario):
+
+    pattern = re.compile(r'^[0-9]+')
+
+    result = pattern.sub('', scenario)
+
+    return result
+
+def get_hints(tokens, scenarios, num_hints, hint_limit):
+    shuffled_scenario = random.sample(scenarios, len(scenarios))
+    # shuffled_scenario = np.random.shuffle(scenarios)
+    hints = []
+    for scenario in shuffled_scenario:
+        if tokens.gpt_get_estimated_cost(scenario, max_tokens=0) <= hint_limit:
+            hints.append(scenario)
+        if len(hints) == num_hints:
+            break
+
+    # length = [tokens.gpt_get_estimated_cost(hint, max_tokens=0) for hint in hints]
+    print('the actual hints are {}'.format(len(hints)))
+    return hints
+
+
+async def chat(Profile_new):
+    chat_completion = await client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": Profile_new,
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+    return chat_completion.choices[0].message.content
 
 
 if __name__ == '__main__':
@@ -79,62 +122,66 @@ if __name__ == '__main__':
 
     generated_scenarios = []
 
-    example_hints = ['Max is going to perform at a concert.', "John's design is rejected by the magzaine.",
+    example_hints = ['Max is going to perform at a concert.', "John's design is rejected by the magzaine.", "Anderson is rejected by his dream school",
                      'Oliver is admitted to his dream university', 'Tom got promoted at the company.', 'Cynthia did not pass the math exam at the end of last term.']
 
-    num_gen = 20
-    target = 100
+    num_gen = 40
+    target = 5000
     num_hints = 5
+    hint_limit = 15
+    tokens = TokenPricer()
 
     while len(generated_scenarios) <= target:
 
-        Profile_new = "You are expert Alex. You are supposed to provide several script scenes." \
+        Profile_new = "You are expert Alex. You are supposed to provide several short, diverse, concise and precise script scenes described in one short sentence." \
                       "Here are some examples of good script scenes for your reference: {} ".format(example_hints) \
                       + (" Please generate {} good script scenes using the aforementioned good script scenes as references."
-                         "The outputs should be separated by newline character '\\n'. Please do not include any number in front of the generated script scene. \n").format(
+                         "The outputs should be separated by newline character '\\n'.\n").format(
             num_gen)
 
-        chat_completion = client.chat.completions.create(
-            messages=[
-                {
-                    "role": "system",
-                    "content": Profile_new,
-                }
-            ],
-            model="gpt-3.5-turbo",
-        )
+        # chat_completion = client.chat.completions.create(
+        #     messages=[
+        #         {
+        #             "role": "system",
+        #             "content": Profile_new,
+        #         }
+        #     ],
+        #     model="gpt-3.5-turbo",
+        # )
 
-        results = chat_completion.choices[0].message.content
+        # results = chat_completion.choices[0].message.content
+
+        results = asyncio.run(chat(Profile_new=Profile_new))
 
         scenarios = results.split('\n')
-        scenarios = [sce for sce in scenarios if sce != ""]
+        scenarios = [check_scenario(sce) for sce in scenarios if sce != ""]
 
-        if len(scenarios) == num_gen and isinstance(scenarios, list):
-            hints = random.sample(scenarios, num_hints)
+        if isinstance(scenarios, list):
+
+            # hints = random.sample(scenarios, num_hints)
+            # hints = np.random.choice(scenarios, size = num_hints, replace=False)
+            if len(scenarios) == num_gen:
+                hints = get_hints(tokens=tokens, scenarios=scenarios, num_hints=num_hints, hint_limit=hint_limit)
+            else:
+                print('generation number exception!')
+                print('scenarios', scenarios)
+                print('num_scenarios', len(scenarios))
+                print('results', results)
+                continue
+
         else:
-            print('generation exception')
-            print('scenarios', scenarios)
-            print('results', results)
+            print('instance error, not a list!')
             continue
 
         example_hints = ' '.join(hints)
-
         generated_scenarios.extend(scenarios)
+
+        # length = [tokens.gpt_get_estimated_cost(i, max_tokens=0) for i in generated_scenarios]
+        # avg_length = sum(length)/80
+
 
     with open('./data/gpt_generated_scenarios.txt', 'w') as f:
         f.writelines('\n'.join(generated_scenarios))
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     print("done!")
